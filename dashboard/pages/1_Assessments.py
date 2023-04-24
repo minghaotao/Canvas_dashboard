b99c30a8234ab56613c2dfa860589766f33159ab
Edward Tao <minghaotao@gmail.com>
2023-04-23 21:37:05 -0400

    Update 1_Assessments.py

--- dashboard/pages/1_Assessments.py
+++ dashboard/pages/1_Assessments.py
@@ -44,30 +44,54 @@ if upload_file is not None:
     def assignment_due():
         st.write(""" Assessment Due Date """)
 
+        # Add dynamic columns
+        columns = st.sidebar.multiselect("Select columns", data['Course_code'].unique().tolist())
+        if columns:
+            # report = data[columns]
+            # selected_course_codes = st.multiselect("Select Course Code(s)", list(course_codes))
+            filtered_data = data[data["Course_code"].isin(columns)]
+        else:
+            #
+            #     selected_course_codes = st.multiselect("Select Course Code(s)", list(course_codes))
+            #
+            #     # Filter the data based on the selected course codes
+            filtered_data = data
+
         tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
 
-        call_days = data['Assignment_due']
-        call_days = pd.DatetimeIndex(call_days.values).strftime('%D').value_counts().sort_index()
-
-        data['Assignment_due'] = pd.to_datetime(data['Assignment_due'], format='%Y/%m/%d')
-
-        data.set_index('Assignment_due', inplace=True)
-
-        report = data['Submission_type'].resample('D').agg({'value_counts'})[
+        filtered_data['Assignment_due'] = pd.to_datetime(data['Assignment_due'], format='%Y/%m/%d')
+        filtered_data.set_index('Assignment_due', inplace=True)
+        report = filtered_data['Submission_type'].resample('D').agg({'value_counts'})[
             'value_counts'].unstack().fillna(0)
-        # fig, ax = plt.subplots()
-        #
-        fig = report.plot.area(fontsize=9, figsize=(9, 7), rot=0, stacked=False)
 
-        # return fig.figure
+        # Add dynamic filters
+        filters = st.sidebar.multiselect("Select assessment types", data['Submission_type'].unique().tolist())
+        if filters:
+            report = report[filters]
 
-        tab1.subheader("A tab with a chart")
+        # Create the chart
+        fig, ax = plt.subplots()
+        report.plot.area(fontsize=9, figsize=(9, 7), rot=0, stacked=False, ax=ax)
+
+        # fig = report.plot.area(fontsize=9, figsize=(9, 7), rot=0, stacked=False)
+
+        tab1.subheader("Filter by Course")
         tab1.pyplot(fig.figure)
 
-        tab2.subheader("A tab with the data")
+        tab2.subheader("Assignment Due Dates")
         tab2.write(report)
 
-        return tab1, tab2
+        tab3, tab4 = st.tabs(["📈 Chart", "🗃 Data"])
+
+        bar_chart = filtered_data['Submission_type'].value_counts()
+
+        tab3.subheader("Filter by Course")
+        tab3.bar_chart(bar_chart)
+
+        tab4.subheader("Number of Submission Types")
+        tab4.write(bar_chart)
+
+        return tab1, tab2, tab3, tab4
 
 
     # st.area_chart(report)
