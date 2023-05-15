a4169124c831fba3c8fa4e02d408543d5f6d8beb
Edward Tao <minghaotao@gmail.com>
2023-05-14 21:57:51 -0400

    Update Home.py

--- dashboard/Home.py
+++ dashboard/Home.py
@@ -3,63 +3,61 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 import matplotlib
+
 matplotlib.style.use('fivethirtyeight')
+
+if "shared" not in st.session_state:
+    st.session_state["shared"] = True
 
 st.write(""" Tool Usage Report """)
 
 upload_file = st.file_uploader("Choose a CSV file")
 
-
-
 if upload_file is not None:
 
-    try:
-        df = pd.read_csv(upload_file)
-        df = df.sort_values(by='number', ascending=False)
+    df = pd.read_csv(upload_file)
+    df = df.fillna(0).set_index('Course Name')
+
+
+    def get_value(column):
+        return df[column].value_counts().drop(0)
+
+
+    def tool_usage():
+
+        df1 = pd.Series([], dtype='int64')
+        for column in df.columns:
+            column = get_value(column)
+
+            df1 = pd.concat([df1, column])
+        df1 = df1.to_frame().reset_index()
+        df1 = df1.rename(columns={0: 'Usage', 'index': 'Tool Name'})
+        ax = df1.sort_values('Usage').plot.barh(x='Tool Name', y='Usage', fontsize=9, figsize=(10, 4), rot=0)
+
+        for patch in ax.patches:
+            ax.text(
+                patch.get_width(),
+                patch.get_y(),
+                " {:,}".format(patch.get_width()),
+                fontsize=10,
+                color='dimgrey'
+            )
+        plt.tight_layout()
 
         tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
-        # data = dict(
-        #     number=[1, 16, 41, 9, 5,3,50,50,13,6,19],
-        #     Tool_Name=["Perusall", "Turnitin", "Brainfuse Tutoring", "Ed Discussion", "InSpace","New Quizzes","Class Recordings","Zoom","Item Banks","VoiceThread","Course Materials @ Penn Libraries"])
+        tab1.subheader("Tool Usage")
+        tab1.pyplot(ax.figure)
 
-        fig = px.funnel(df, x='number', y='Tool_Name')
+        tab2.subheader("Raw Data")
+        tab2.write(df1)
 
-        tab1.subheader("A tab with a chart")
-        tab1.plotly_chart(fig, theme="streamlit", use_container_width=True)
+        return tab1, tab2
 
-        tab2.subheader("A tab with the data")
-        tab2.write(df)
-    except:
-        pass
 
+    tool_usage()
+
+    # df = df.sort_values(by='number', ascending=False)
+
+    # fig = px.funnel(df, x='number', y='Tool_Name')
 
 # st.plotly_chart(fig, theme=None, use_container_width=True)
-
-
-
-# matplotlib.style.use('fivethirtyeight')
-# ax1 = df.sort_values('Response_Rate%').plot.barh(x="Courses", y="Response_Rate%", fontsize=9, figsize=(10, 7))
-#
-#     for patch in ax1.patches:
-#         ax1.text(
-#             patch.get_width(),
-#             patch.get_y(),
-#             " {:,}%".format(patch.get_width()),
-#             fontsize=10,
-#             color='dimgrey'
-#         )
-#
-#     plt.title(f'late_24Hours_Response_Rate-Week of {ed_reports.filt_week}')
-#
-#     plt.tight_layout()
-#
-# st.pyplot(ax1)
-#
-# df = pd.read_csv(
-#     'https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-
-# fig1 = px.scatter(df, x="gdp per capita", y="life expectancy",
-#                  size="population", color="continent", hover_name="country",
-#                  log_x=True, size_max=60)
-#
-# st.plotly_chart(fig1, theme=None, use_container_width=True)
